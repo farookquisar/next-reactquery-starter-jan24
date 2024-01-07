657f48dda17dff65cc80a8b50c040d2435f7e19b
farookquisar <farook.quisar@gmail.com>
2024-01-07 13:48:33 +0300

    1

--- src/app/api/getMovies.tsx
+++ src/app/api/getMovies.tsx
@@ -3,13 +3,13 @@ async function getData() {
 		method: "GET",
 		headers: {
 			accept: "application/json",
-			Authorization: `Bearer ${process.env.API_TOKEN}`,
+			Authorization: `Bearer${process.env.API_TOKEN}`,
 		},
 	};
 
 	const url: string =
 		"https://api.themoviedb.org/3/tv/popular?language=en-US&page=1";
-	const response: Promise<any> = fetch(url, options)
+	const response = fetch(url, options)
 		.then((response) => response.json())
 		.catch((err) => console.error(err));
 
