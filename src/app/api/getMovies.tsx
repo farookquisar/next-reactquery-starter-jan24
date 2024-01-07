fbcf81695bc3fbf50487efd37985b159e0397109
farookquisar <farook.quisar@gmail.com>
2024-01-07 13:24:45 +0300

    1

--- src/app/api/getMovies.tsx
+++ src/app/api/getMovies.tsx
@@ -1,24 +1,22 @@
 async function getData() {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer ...",
-    },
-  };
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization: process.env.API_KEY,
+		},
+	};
 
-  const response = fetch(
-    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-    options
-  )
-    .then((response) => response.json())
-    .catch((err) => console.error(err));
+	const url: string =
+		"https://api.themoviedb.org/3/tv/popular?language=en-US&page=1";
+	const response: Promise<any> = fetch(url, options)
+		.then((response) => response.json())
+		.catch((err) => console.error(err));
 
-  return response;
+	return response;
 }
 
 export default async function getMovies() {
-  const data = await getData();
-  return data;
+	const data = await getData();
+	return data;
 }
