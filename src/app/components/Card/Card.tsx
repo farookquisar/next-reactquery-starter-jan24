b25a4b35323177fcaaed9dca1a232be71d6535cd
farookquisar <farook.quisar@gmail.com>
2024-01-07 14:48:22 +0300

    1

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import Image from "next/image";
-
-const Card = ({ title, image }: { title: string; image: string }) => {
-	return (
-		<div className="p-5">
-			<h2 className="text-wrap py-3 uppercase">{title}</h2>
-			<Image src={"https://image.tmdb.org/t/p/w500" + image} alt={title} />
-		</div>
-	);
+const Card = ({ title, image }: { title: string, image:string }) => {
+  return (
+    <div className="p-5">
+      <h2 className="text-wrap py-3 uppercase">{title}</h2>
+      <img src={"https://image.tmdb.org/t/p/w500"+image} />
+    </div>
+  );
 };
 
 export default Card;
